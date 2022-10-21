af60bbbf6e4f99765d5b5ce55e4afaa5e415c9b1
JB <47344398+jbsuratos-tt5@users.noreply.github.com>
2022-10-21 15:43:34 +0800

    Add files via upload

--- exercise-4.js
+++ exercise-4.js
@@ -1,13 +1,13 @@
 // Find and fix the syntax error of the code below
 
-async function sleep(brand) {
-     console.log(`${brand} executed!`);
+async function sleep() {
+     return new Promise(resolve => setTimeout(resolve, 1000));
 }
 
 async function executeLoop(list) {
      list.forEach(brand => {
           console.log('Executing... ' + brand);
-          await sleep(brand);
+          await sleep();
      });
 }
 
